5ffe01d1df9f2c2a5a1ad529102a14bd0f612925
CCDM17 <ccdm17@CCDM17-2.local>
2023-11-08 12:58:37 -0700

    Practica01 Terminada

--- randomuser/randomuser.js
+++ randomuser/randomuser.js
@@ -1,10 +1,38 @@
+var numero = 0;
+var cantidad = 0;
+var datos = [];
+
 async function apirandomuser() {
-    const respuesta = await fetch("https://randomuser.me/api/");
-    const datos = await respuesta.json();
+    numero = 0;
+    cantidad  = document.getElementById("cantidad").value;
+    const respuesta = await fetch("https://randomuser.me/api?results="+cantidad);
+    datos = await respuesta.json();
+    console.log(datos)
+    muestra();
+}
+
+function anterior(){
+    numero--;
+    if(numero < 0) {
+        numero = cantidad;
+    }
+    muestra();
+}
+
+function siguiente(){
+    numero++;
+    if(numero == cantidad){
+        numero = numero-cantidad;
+    }
+    muestra();
+}
+
+function muestra() {
+    console.log('Arreglo en la posicion '+numero);
     let foto = document.getElementById('foto');
     let nombre = document.getElementById('nombre');
     let telefono = document.getElementById('telefono');
-    foto.setAttribute('src', datos.results[0].picture.large)
-    nombre.innerHTML = datos.results[0].name.first+" "+datos.results[0].name.last;
-    telefono.innerHTML = datos.results[0].phone;
+    foto.setAttribute('src', datos.results[numero].picture.large)
+    nombre.innerHTML = datos.results[numero].name.first+" "+datos.results[numero].name.last;
+    telefono.innerHTML = datos.results[numero].phone;
 }
