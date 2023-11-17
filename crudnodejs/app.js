e0fc115975e3cc9b2f2dd819a5b39605d28c0ffa
CCDM17 <ccdm17@CCDM17-2.local>
2023-11-17 12:59:26 -0700

    Crud nodejs

---
+++ crudnodejs/app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var express = require('mysql');
+var app = express();
+app.use(express.json);
+
+
+// Configurar la conexion
+var conexion = mysql.create({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'pw1213'
+});
+
+// Probar conexion
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log("Conectado a la base de datos");
+    }
+});
+
+app.get("/", function(req, res) {
+    res.send("<h1>Ruta de inicio</h1>");
+});
+
+app.get("api/maestros", function(req, res){
+    conexion.query("SELECT * FROM maestros", (error, filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas)
+        }
+    });
+});
+
+app.get("api/maestros", function(req, res){
+    conexion.query("SELECT * FROM maestros WHERE clave = ? LIMIT 1", [request.param.id], (error, filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
+
+// Encender el servidor
+app.listen('3000', (req, res) => {
+    console.log("Servidor en puerto 3000");
+});
+
+app.post("api/maestros", (req, res) => {
+    let data = { clave: req.body.cla,
+    nombre: req.body.nom,
+    departamento: req.body.dep,
+    estatus: req.body.est}
+    let sql = "INSERT INTO maestros SET ?";
+    conexion.query(sql, data, (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+app.put('api/maestros/:id', (req,res)=>{
+    let clave = req.params.id;
+    let nombre = req.params.nom;
+    let departamento  = req.params.dep;
+    let estatus = req.body.est;
+    let sql = "UPDATE maestros SET nombre = ?, departamento =?, estatus=? WHERE clave=?";
+    conexion.query(sql, [nombre, departamento,estatus], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
